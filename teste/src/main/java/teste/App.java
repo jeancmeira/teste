b165b120537c1c5013ee76eaca64e1acf8dfd308
JGuy <JGuy@dexter>
2013-08-20 03:09:45 -0300

    yhjhjhgjhg

--- teste/src/main/java/teste/App.java
+++ teste/src/main/java/teste/App.java
@@ -7,7 +7,7 @@ package teste;
 public class App 
 {
     public int soma(int n1, int n2) {
-        return n1 - n2;
+        return n1 + n2;
     }
     
     public static void main(String[] args) {
