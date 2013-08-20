bb3130cc36bc383579239e4e134e71f33fc9bda1
JGuy <JGuy@dexter>
2013-08-20 02:53:50 -0300

    Teste...

---
+++ teste/src/main/java/teste/App.java
@@ -0,0 +1,16 @@
+package teste;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public int soma(int n1, int n2) {
+        return n1 - n2;
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ teste/src/test/java/teste/AppTest.java
@@ -0,0 +1,40 @@
+package teste;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        int result = new App().soma(1, 2);
+        
+        assertTrue( result == 3 );
+    }
+}
